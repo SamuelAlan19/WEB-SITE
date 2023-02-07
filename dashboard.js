d81ba932af29de5f0b45b1df686c87979cdec4c8
Samuel <samuelsantos@sescmg.com.br>
2023-02-07 15:16:38 -0300

    UPD VIDEOS

--- dashboard.js
+++ dashboard.js
@@ -1,4 +1,4 @@
-$('.filter-btn').on('click', function() {
+$('.filter-btn').on('click', function () {
 
     let type = $(this).attr('id');
     let boxes = $('.project-box');
@@ -7,33 +7,33 @@ $('.filter-btn').on('click', function() {
     $(this).addClass('active');
 
 
-    if(type=='dsg-btn'){
+    if (type == 'dsg-btn') {
         eachBoxes('dsg', boxes)
-    } else if(type == 'dev-btn') {
+    } else if (type == 'dev-btn') {
         eachBoxes('dev', boxes);
-    }else if(type == 'seo-btn')  {
+    } else if (type == 'seo-btn') {
         eachBoxes('seo', boxes);
-    }else {
+    } else {
         eachBoxes('all', boxes);
 
     }
 
 });
 
-function eachBoxes(type, boxes){
+function eachBoxes(type, boxes) {
 
-    if(type == 'all'){
-    $(boxes).fadeIn();
-} else {
-    $(boxes).each(function() {
-        if(!$(this).hasClass(type)){
-            $(this).fadeOut('slow');
-        } else {
-           $(this).fadeIn(); 
-        }
-    });
+    if (type == 'all') {
+        $(boxes).fadeIn();
+    } else {
+        $(boxes).each(function () {
+            if (!$(this).hasClass(type)) {
+                $(this).fadeOut('slow');
+            } else {
+                $(this).fadeIn();
+            }
+        });
+    }
 }
-} 
 
 let navBtn = $('.nav-item');
 
@@ -46,19 +46,19 @@ let contactSection = $('#contact-area');
 
 let scrollTo = '';
 
-$(navBtn).click(function() {
-    
+$(navBtn).click(function () {
+
     let btnId = $(this).attr('id');
     console.log(btnId);
 
-    if(btnId == 'about-menu') {
+    if (btnId == 'about-menu') {
         scrollto = aboutSection;
     } else if (btnId == 'services-menu') {
         scrollto = servicesSection;
     } else if (btnId == 'team-menu') {
         scrollto = teamSection;
     } else if (btnId == 'portifolio-menu') {
-        scrollto = portifolioSection; 
+        scrollto = portifolioSection;
     } else if (btnId == 'contact-menu') {
         scrollto = contactSection;
     } else {
@@ -70,3 +70,15 @@ $(navBtn).click(function() {
     }, 1000);
 
 });
+
+let sql = document.querySelector('.video1'); 
+sql.addEventListener('click', () => { window.location.href = "https://www.youtube.com/playlist?list=PLHz_AreHm4dkBs-795Dsgvau_ekxg8g1r"; })
+
+let php = document.querySelector('.video2'); 
+php.addEventListener('click', () => { window.location.href = "https://www.youtube.com/playlist?list=PLHz_AreHm4dm4beCCCmW4xwpmLf6EHY9k"; })
+
+let js = document.querySelector('.video3'); 
+js.addEventListener('click', () => { window.location.href = "https://www.youtube.com/playlist?list=PLntvgXM11X6pi7mW0O4ZmfUI1xDSIbmTm"; })
+
+let html = document.querySelector('.video4'); 
+html.addEventListener('click', () => { window.location.href = "https://www.youtube.com/playlist?list=PLHz_AreHm4dkZ9-atkcmcBaMZdmLHft8n"; })
